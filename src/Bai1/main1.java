5ac449d370d3836e958b8f37b502d1f97593aa4c
TranHuyThinh <tranhuythinh97@gmail.com>
2020-04-14 15:42:36 +0700

    Khoi tao

---
+++ src/Bai1/main1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai1;
+
+/**
+ *
+ * @author TranHuyThinh
+ */
+public class main1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
